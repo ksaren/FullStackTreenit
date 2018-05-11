a946a20858130e9dd3da15f1aae00c410dbd4d74
Kaisa Sarén <kaisa.saren@gmail.com>
2018-05-11 13:59:50 +0300

    2_notes demo done

--- PART_2/demo2_notes/src/App.js
+++ PART_2/demo2_notes/src/App.js
@@ -1,17 +1,71 @@
 import React from 'react'
 import Note from './components/Note'
 
-const App = (props) => {
-  const { notes } = props;
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      notes: props.notes,
+      newNote: 'uusi muistiinpano...',
+      showAll: true,
+    }
+  } 
+addNote = (event) => {
+  event.preventDefault()
+  const noteObject = {
+    content: this.state.newNote,
+    date: new Date().new,
+    important: Math.random() > 0.5,
+    id: this.state.notes.length + 1
+  }
+  const notes = this.state.notes.concat(noteObject)
+  this.setState({
+    notes,
+    newNote: ''
+  })
+}
+
+handleNoteChange = (event) =>{
+  console.log(event.target.value)
+  this.setState({newNote: event.target.value})
+}
+
+toggleVisible = () => {
+  this.setState({showAll: !this.state.showAll})
+}
+
+  render() {
+
+    const notesToShow =
+      this.state.showAll ?
+        this.state.notes:
+        this.state.notes.filter(note => note.important)
   
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => <Note key={note.id} text={note.content}/>)}
-      </ul>
-    </div>
-  )
+
+    const label = this.state.showAll ? 'only important': 'all'
+
+    return (
+      <div>
+        <h1>Notes</h1>
+        <div>
+          <button onClick={this.toggleVisible}>
+          {label}
+          </button>
+        </div>
+        <ul>
+          {notesToShow.map(note => <Note key={note.id} text={note.content}/>)}
+        </ul>
+        <form onSubmit={this.addNote}>
+          <input 
+            value={this.state.newNote}
+            onChange={this.handleNoteChange}
+          />
+          <button type='submit'>save</button>
+        </form>
+      </div>
+    )
+  }
+  
 }
 
 export default App
